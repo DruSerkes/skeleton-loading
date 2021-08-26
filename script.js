e975f8bc7530ac703c1ea8ab906f1f1ab9919bf8
Dru Serkes <dru@zephyrx.com>
2021-08-26 10:28:46 -0400

    remove animation after load to optimize

--- script.js
+++ script.js
@@ -9,14 +9,21 @@ for (let i = 0; i < 12; i++) {
   grid.append(clone);
 };
 
+const removeSkeletonClasses = (el) => el.classList.remove('skeleton', 'skeleton-text');
+
 fetch(POSTS_URL)
   .then(res => res.json())
   .then(posts => {
     grid.innerHTML = '';
+
     posts.forEach(post => {
       const clone = template.content.cloneNode(true);
       clone.querySelector('.title').innerText = post.title;
       clone.querySelector('.body').innerText = post.body;
       grid.append(clone);
     });
+
+    // remove skeleton animation to optimize
+    const skeletonElements = document.querySelectorAll('.skeleton');
+    skeletonElements.forEach(el => el.addEventListener('load', () => removeSkeletonClasses(el)));
   });
