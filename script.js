1e27a9269c817f1c157ffaa2ae46f92e2d2d67e7
Dru Serkes <dru@zephyrx.com>
2021-08-26 00:10:31 -0400

    add skeleton loading animations

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const template = document.querySelector('template');
 fetch(POSTS_URL)
   .then(res => res.json())
   .then(posts => {
+    grid.innerHTML = '';
     posts.forEach(post => {
       const clone = template.content.cloneNode(true);
       clone.querySelector('.title').innerText = post.title;
