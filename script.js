64831f147e8fbc0620372f033e75510e5d8d12ef
Dru Serkes <dru@zephyrx.com>
2021-08-25 23:53:45 -0400

    add template for styled cards with placeholder data

---
+++ script.js
@@ -0,0 +1,15 @@
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const grid = document.querySelector('.grid');
+const template = document.querySelector('template');
+
+fetch(POSTS_URL)
+  .then(res => res.json())
+  .then(posts => {
+    posts.forEach(post => {
+      const clone = template.content.cloneNode(true);
+      clone.querySelector('.title').innerText = post.title;
+      clone.querySelector('.body').innerText = post.body;
+      grid.append(clone);
+    });
+  });
