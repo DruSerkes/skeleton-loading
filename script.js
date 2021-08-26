60167f50cc3d4ada3f369e9e08ec857cc73874fb
Dru Serkes <dru@zephyrx.com>
2021-08-26 08:45:28 -0400

    dynamically add skeleton cards while loading

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 const grid = document.querySelector('.grid');
 const template = document.querySelector('template');
 
+// Show 12 skeleton loading cards while we wait 
+for (let i = 0; i < 12; i++) {
+  const clone = template.content.cloneNode(true);
+  grid.append(clone);
+};
+
 fetch(POSTS_URL)
   .then(res => res.json())
   .then(posts => {
